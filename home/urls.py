df0016aa0a82c64774e4faf587ce17b519bac63d
killerwhalee <killerwhalee@kaist.ac.kr>
2024-02-24 17:01:10 +0900

    Added menu page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'home'
+app_name = "home"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("menu", views.menu, name="menu"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, "home/index.html")
+
+
+def menu(request):
+    return render(request, "home/menu.html")
