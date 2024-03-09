d68e72fb859986b4660df7219b0b586bf8e05400
killerwhalee <killerwhalee@kaist.ac.kr>
2024-03-09 16:26:43 +0900

    Improved code structure

--- common/utils.py
+++ common/utils.py
@@ -1,16 +1,26 @@
-# Encrypting file name
+"""
+Utilities
+
+This file defines utilities for general use.
+Add any functions to call from any applications.
+
+"""
+
+
 def uuid_filepath(instance, filename):
+    """
+    Generate filepath regarding instance
+
+    File name is renamed into uuid
+
+    """
+
     import os
     from uuid import uuid4
 
-    app_name = instance.__class__._meta.app_label
+    app_name = instance.__class__._meta.app_label.lower()
     model_name = instance.__class__.__name__.lower()
     uuid_name = uuid4().hex
-    extension = os.path.splitext(filename)[-1].lower()
-    return "/".join(
-        [
-            app_name,
-            model_name,
-            uuid_name + extension,
-        ]
-    )
+    _, ext = os.path.splitext(filename)
+
+    return f"{app_name}/{model_name}/{uuid_name}{ext.lower()}"
