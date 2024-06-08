b7cb029a775a83959448280d7c44d1027dff27f4
killerwhalee <killerwhalee@kaist.ac.kr>
2024-06-08 16:22:12 +0900

    Updated settings

--- core/settings.py
+++ core/settings.py
@@ -1,37 +1,32 @@
 """
-Django Base Configuration
+OwlChem Backend Configuration
 
-WARNING: Do not use this setting for deployment!
+This is the settings for OwlChem backend, written in python django.
 
-This is the default setting for django project.
-You can run a test server only using this configuration.
+Configuration which needs to be secured are separated using environment variable.
+Define your own environment variable file to configure those settings.
 
-If you need some change in configuration,
-do not change this file directly.
-
-Rather, create other configuration file and import this file.
-Then you can freely override the configuration.
+Also OwlChem supports default value for those, which is safe to use.
+But please remind that default value is only for development.
 
 Configured by killerwhalee
-
-Github: https://github.com/killerwhalee/healingmentor
 
 """
 
 from pathlib import Path
-import dotenv, os
+import os
 
-# Load Environment Variables
+# Host Name
 
-dotenv.load_dotenv()
+HOST_NAME = os.environ.get("HOST_NAME", "localhost")
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Base Directory
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# Secret Key for Django
 
 from django.core.management.utils import get_random_secret_key
 
@@ -40,33 +35,55 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = int(os.environ.get("DJANGO_DEBUG", 0))
+DEBUG = bool(int(os.environ.get("DJANGO_DEBUG", 0)))
 
 
 # Allowed Hosts
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split()
+ALLOWED_HOSTS = [f".{HOST_NAME}", "localhost"]
 
 
-# Application definition
+# Trusted CSRF Origins
+
+CSRF_TRUSTED_ORIGINS = [f"https://*.{HOST_NAME}", "http://localhost"]
+
+
+# CORS Whitelist
+
+CORS_ALLOWED_ORIGINS = [
+    f"https://api.{HOST_NAME}",
+    f"https://{HOST_NAME}",
+    "http://localhost:3000",
+]
+
+
+# Application Definition
 
 INSTALLED_APPS = [
-    # System Application
+    # System built-in application
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # General Purpose Application
+    # System vendor applications
+    "rest_framework",
+    "corsheaders",
+    "rest_framework_simplejwt",
+    # User Application
     "user.apps.UserConfig",
-    # Applications
     "home.apps.HomeConfig",
     "evaluate.apps.EvaluateConfig",
     "tools.apps.ToolsConfig",
+    "database.apps.DatabaseConfig",
 ]
 
+
+# Middleware Setting
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -76,7 +93,56 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+
+# Django REST Framework Settings
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        ["rest_framework_simplejwt.authentication.JWTAuthentication"]
+        # Use session authentication only in debug mode
+        + ["rest_framework.authentication.SessionAuthentication"] * DEBUG
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+    "DEFAULT_RENDERER_CLASSES": (
+        ["rest_framework.renderers.JSONRenderer"]
+        # Use browsable api only in debug mode
+        + ["rest_framework.renderers.BrowsableAPIRenderer"] * DEBUG
+    ),
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
+}
+
+
+# Django JWT Settings
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours=2),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer", "bearer"),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+}
+
+
+# Url Configuration
+
 ROOT_URLCONF = "core.urls"
+
+
+# Templates Setting
 
 TEMPLATES = [
     {
@@ -94,17 +160,28 @@ TEMPLATES = [
     },
 ]
 
+
+# WSGI Setting
+
 WSGI_APPLICATION = "core.wsgi.application"
 
 
-# Database
+# Database Setting
+# Default is SQLite3. Override the setting for other option.
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get(
+            "DJANGO_DATABASE_ENGINE", "django.db.backends.sqlite3"
+        ),
+        "NAME": os.environ.get("DJANGO_DATABASE_NAME", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("DJANGO_DATABASE_USER", "user"),
+        "PASSWORD": os.environ.get("DJANGO_DATABASE_PASSWORD", "password"),
+        "HOST": os.environ.get("DJANGO_DATABASE_HOST", "localhost"),
+        "PORT": os.environ.get("DJANGO_DATABASE_PORT", "5432"),
     }
 }
+
 
 # Customized User Model
 
@@ -135,6 +212,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Registration
+# Redirect to Root for default.
 
 LOGIN_REDIRECT_URL = "/"
 
@@ -143,28 +221,31 @@ LOGOUT_REDIRECT_URL = "/"
 
 # Internationalization
 
-LANGUAGE_CODE = "ko-kr"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Asia/Seoul"
+
+DATE_FORMAT = "Y.m.d"
+
+DATETIME_FORMAT = "Y.m.d h:i A"
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (For Admin Only)
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-# STATIC_ROOT = BASE_DIR / "_static"
-STATICFILES_DIRS = [BASE_DIR / "_static"]
+STATIC_ROOT = BASE_DIR / "_static"
 
 
 # User Media
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 
 MEDIA_ROOT = BASE_DIR / "_media"
 
@@ -179,21 +260,16 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse",
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins", "file"],
+            "level": "INFO",
         },
-        "require_debug_true": {
-            "()": "django.utils.log.RequireDebugTrue",
-        },
-    },
-    "formatters": {
         "django.server": {
-            "()": "django.utils.log.ServerFormatter",
-            "format": "[{server_time}] {message}",
-            "style": "{",
+            "handlers": ["django.server"],
+            "level": "INFO",
+            "propagate": False,
         },
-        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
     },
     "handlers": {
         "console": {
@@ -216,20 +292,25 @@ LOGGING = {
             "filters": ["require_debug_false"],
             "class": "logging.handlers.RotatingFileHandler",
             "filename": BASE_DIR / "_logs/site.log",
-            "maxBytes": 64 * 1024 * 1024,  # 64 MB
-            "backupCount": 100,
+            "maxBytes": 1024 * 1024,  # 1 MB
+            "backupCount": 256,
             "formatter": "standard",
         },
     },
-    "loggers": {
-        "django": {
-            "handlers": ["console", "mail_admins", "file"],
-            "level": "INFO",
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
         },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
         "django.server": {
-            "handlers": ["django.server"],
-            "level": "INFO",
-            "propagate": False,
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
         },
+        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
     },
 }
